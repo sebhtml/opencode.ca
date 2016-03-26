aedd24041939cd2c5e0bbf034c343102b0ee7fd2
Sébastien Boisvert <seb@boisvert.info>
2016-03-25 21:50:27 -0400

    load files
    Signed-off-by: Sébastien Boisvert <seb@boisvert.info>

--- 2016-03-29,XXIX/demo.el
+++ 2016-03-29,XXIX/demo.el
@@ -1,10 +1,44 @@
 
+
+(defun
+    get-string-from-file (file-path)
+  (with-temp-buffer
+    (insert-file-contents file-path)
+    (buffer-string)))
+
+(defun
+    load-document (file-path)
+
+  (let
+      ((content (get-string-from-file file-path)))))
+
+(defun
+    align-documents (document-a document-b)
+  (+ 2 2))
+
 (defun
     demo ()
 
-  (message "Hello world")
+  (let
+      (
+       (gnu-emacs-wikipedia-page
+        (load-document
+        "Internet/https:-SLASH-SLASH-en.wikipedia.org-SLASH-wiki-SLASH-GNU_Emacs"))
 
-  )
+       (spacemacs-github-page
+        (load-document
+        "Internet/https:-SLASH-SLASH-github.com-SLASH-syl20bnr-SLASH-spacemacs"))
+
+       (spacemacs-twitter-page
+        (load-document
+        "Internet/https:-SLASH-SLASH-twitter.com-SLASH-spacemacs"))
+
+       (my-text
+        (load-document
+        "my-text.txt"))
+       )
+     (align-documents gnu-emacs-wikipedia-page my-text)
+  ))
 
 
 (demo)
